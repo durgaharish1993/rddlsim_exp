1d8520827685dec592a69d7e82fb60b8b4e22f5c
dimbul <durgaharish1993@gmail.com>
2018-10-06 22:51:03 -0700

    Added the functionality for calculating the objective value by acting differently in the environment at step 0, Added new Functions : 1. getActionNotEqualGurobi 2. addBoolDiffAction, Created a new high level variable : DO_SOMETHING_ELSE_GUROBI- this toggle to act differently and calculates the new objective

--- src/rddl/det/comMipNew/HOPExplainModule.java
+++ src/rddl/det/comMipNew/HOPExplainModule.java
@@ -6,18 +6,18 @@ import gurobi.*;
 import rddl.EvalException;
 import rddl.RDDL;
 import rddl.State;
+import rddl.policy.RandomPolicy;
 import util.Pair;
 
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+
+import static rddl.RDDL.EXPR.name_map;
 
 public class HOPExplainModule extends HOPPlannerNew {
 
@@ -27,14 +27,21 @@ public class HOPExplainModule extends HOPPlannerNew {
     protected String REWARD_KEY = "_Reward";
     protected String PLAN_PATH  = "./Explanation_Output/HOP_Plans/";
     protected String VISUAL_CROSSING_DOMAIN_FOLDER = "./Explanation_Output/HOP_Visual_Plans/";
-
+    protected String PREFIX_VIZ_FILE = null;
+    protected boolean DO_SOMETHING_ELSE_GUROBI = true;
+    protected Writer writer_obj = null;
 
     public  HOPExplainModule(String rddl_filename, String n_futures, String n_lookahead, String inst_name,
-                            String gurobi_timeout, String future_gen_type, String hindsight_strat, String rand_seed,String plan_file_name) throws Exception{
+                            String gurobi_timeout, String future_gen_type, String hindsight_strat, String rand_seed,String plan_file_name, String prefix_path, String explanation_objectives_path) throws Exception{
 
         super(rddl_filename,n_futures,n_lookahead,inst_name,gurobi_timeout,future_gen_type,hindsight_strat,rand_seed);
         System.out.println("dkfjkdjfkdjkfjd");
         this.PLAN_PATH = this.PLAN_PATH + plan_file_name;
+        this.PREFIX_VIZ_FILE = prefix_path;
+
+        this.writer_obj = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(explanation_objectives_path), "utf-8"));
+
 
         //Need to think about the parameter for explaination.
 
@@ -47,9 +54,6 @@ public class HOPExplainModule extends HOPPlannerNew {
             if(DO_NPWL_PWL){
                 checkNonLinearExpressions(state);
             }
-
-
-
             for(int i=0 ; i<10 ; i++){
                 ArrayList<RDDL.PVAR_INST_DEF> actions = null;
                 try {
@@ -74,10 +78,6 @@ public class HOPExplainModule extends HOPPlannerNew {
 
 
             }
-
-
-            //Need to get information on each state in future and their actions.
-
     }
 
 
@@ -90,11 +90,17 @@ public class HOPExplainModule extends HOPPlannerNew {
             System.out.println("----Initializing Gurobi model----");
             firstTimeModel(subs);
         }
-        try{
-            translateCPTs( subs, static_grb_model , false );
-            translateReward( static_grb_model , false);
-            translateInitialState( static_grb_model, subs );
+        try {
+            translateCPTs(subs, static_grb_model, false);
+            translateReward(static_grb_model, false);
+            translateInitialState(static_grb_model, subs);
+        } catch (Exception e ){
+            System.out.println("--- There is problem with the translating the domain to gurobi");
+            throw e ;
 
+        }
+
+        try{
             int exit_code = -1;
             try{
                 System.out.println("----STARTING OPTIMIZATION----");
@@ -119,11 +125,54 @@ public class HOPExplainModule extends HOPPlannerNew {
                 }
             }
             /////////////////////////////////
+
             Pair<ArrayList<HashMap<String,HashMap<RDDL.LCONST,Object>>>,ArrayList<ArrayList<String>>> plans_info = decomGRBPlans(static_grb_model);
             ArrayList<HashMap<String,HashMap<RDDL.LCONST,Object>>> all_future_plans = plans_info._o1;
             ArrayList<ArrayList<String>> all_futures_str = plans_info._o2;
-            vizPlansToFolder(all_future_plans,"VIZ_CONSENSUS_PLANS_F_5_L_5_N_");
-            printPlanToFile(all_futures_str,"CONSENSUS_PLANS_F_5_L_5.csv");
+            vizPlansToFolder(all_future_plans,PREFIX_VIZ_FILE);
+            printPlanToFile(all_futures_str,null);
+            double obj_val = static_grb_model.get(GRB.DoubleAttr.ObjVal);
+
+
+            ArrayList<RDDL.PVAR_INST_DEF> ret2 = null;
+            if(DO_SOMETHING_ELSE_GUROBI){
+                HashSet<RDDL.PVAR_INST_DEF> action_not_same = getActionNotEqualGurobi(s,0);
+                for(int k =0 ; k< future_TERMS.size() ; k++){
+                    static_grb_model = addBoolDiffAction(static_grb_model,action_not_same,k);
+                }
+                static_grb_model.reset();
+                try{
+                    System.out.println("----STARTING OPTIMIZATION----");
+                    grb_env.set( GRB.DoubleParam.TimeLimit, TIME_LIMIT_MINS*60 );
+                    exit_code = goOptimize( static_grb_model);
+                }
+                catch( GRBException exc ){
+                    int error_code = exc.getErrorCode();
+                }
+
+                double obj_val1 = static_grb_model.get(GRB.DoubleAttr.ObjVal);
+
+                ret  = outputResults( static_grb_model);
+                modelSummary( static_grb_model );
+                ret2 = getRootActions(ret,s);
+                Pair<ArrayList<HashMap<String,HashMap<RDDL.LCONST,Object>>>,ArrayList<ArrayList<String>>> plans_info1 = decomGRBPlans(static_grb_model);
+                ArrayList<HashMap<String,HashMap<RDDL.LCONST,Object>>> all_future_plans1 = plans_info1._o1;
+                ArrayList<ArrayList<String>> all_futures_str1 = plans_info1._o2;
+                vizPlansToFolder(all_future_plans1,"Second_plan_F_1_L_5_");
+                printPlanToFile(all_futures_str1,"./Explanation_Output/HOP_Plans/second_plan_F_1_L_5.csv");
+
+
+                String cur_state_val = s._state.toString().replace(",",":");
+                String optimal_action = ret1.toString().replace(",",":");
+                String not_optimal_action = ret2.toString().replace(",",":");
+                String obj_string_1 = String.valueOf(obj_val);
+                String obj_string_2 = String.valueOf(obj_val1);
+                writer_obj.write(cur_state_val+","+optimal_action+"," + obj_string_1 + "," + not_optimal_action + "," + obj_string_2 +"\n");
+                writer_obj.flush();
+
+            }
+
+
             cleanUp(static_grb_model);
             return ret1;
 
@@ -140,7 +189,13 @@ public class HOPExplainModule extends HOPPlannerNew {
     public void  vizPlansToFolder(ArrayList<HashMap<String,HashMap<RDDL.LCONST,Object>>> plans,String file_prefix) throws Exception{
         CrossingWorldUI print_obj =new CrossingWorldUI();
         for(int i=0 ; i<plans.size();i++){
-            String vizPlan = print_obj.runPlan(plans.get(i),this.lookahead);
+            String vizPlan = "";
+            try{
+                vizPlan = print_obj.runPlan(plans.get(i),this.lookahead);
+            } catch (Exception e){
+                System.out.println("--The Plan Could not produce Visualization --");
+
+            }
 
             try (Writer writer = new BufferedWriter(new OutputStreamWriter(
                     new FileOutputStream(VISUAL_CROSSING_DOMAIN_FOLDER+file_prefix+"_"+String.valueOf(i)+".txt"), "utf-8"))) {
@@ -153,8 +208,6 @@ public class HOPExplainModule extends HOPPlannerNew {
         }
 
     }
-
-
 
 
     public Pair<ArrayList<HashMap<String,HashMap<RDDL.LCONST,Object>>>,ArrayList<ArrayList<String>>> decomGRBPlans(final GRBModel static_grb_model) throws Exception{
@@ -173,9 +226,7 @@ public class HOPExplainModule extends HOPPlannerNew {
     }
 
 
-
-
-    public void printPlanToFile(ArrayList<ArrayList<String>> plans, String file_name) throws Exception{
+    public void printPlanToFile(ArrayList<ArrayList<String>> plans, String file_path) throws Exception{
         ArrayList<String> concat_list = new ArrayList<>();
         for(int i=0 ; i<plans.size();i++){
             for(int j=0 ;j<plans.get(i).size();j++){
@@ -188,12 +239,25 @@ public class HOPExplainModule extends HOPPlannerNew {
             }
         }
 
-        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(PLAN_PATH+file_name), "utf-8"))) {
-            for(int i=0;i<concat_list.size();i++){
-                writer.write(concat_list.get(i)+"\n");
+        if(file_path==null){
+            try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(PLAN_PATH), "utf-8"))) {
+                for(int i=0;i<concat_list.size();i++){
+                    writer.write(concat_list.get(i)+"\n");
+                }
             }
+
+        }else{
+            try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(file_path), "utf-8"))) {
+                for(int i=0;i<concat_list.size();i++){
+                    writer.write(concat_list.get(i)+"\n");
+                }
+            }
+
+
         }
+
     }
 
 
@@ -268,6 +332,97 @@ public class HOPExplainModule extends HOPPlannerNew {
     }
 
 
+    public HashSet<RDDL.PVAR_INST_DEF> getActionNotEqualGurobi(State s,int future_val) throws Exception{
+        RandomPolicy random_policy = new RandomPolicy(s);
+        ArrayList<ArrayList<RDDL.PVAR_INST_DEF>> legalActions = new ArrayList<>();
+        HashMap<RDDL.PVAR_NAME, ArrayList<ArrayList<RDDL.LCONST>>> src = new HashMap<>();
+        src.putAll(rddl_action_vars);
+        Map<RDDL.EXPR, Double > ret = new HashMap<RDDL.EXPR, Double >();
+        RDDL.LCONST time_term  = new RDDL.OBJECT_VAL("time0");
+        RDDL.LCONST future_term = new RDDL.OBJECT_VAL("future"+String.valueOf(future_val));
+        for(RDDL.PVAR_NAME key : src.keySet()){
+            ArrayList<ArrayList<RDDL.LCONST>> vals = src.get(key);
+            for(int i=0 ; i<vals.size();i++){
+                ArrayList<RDDL.LCONST> terms = vals.get(i);
+                RDDL.EXPR action_var = new RDDL.PVAR_EXPR(key._sPVarName,terms)
+                        .addTerm(TIME_PREDICATE, constants, objects, hmtypes, hm_variables )
+                        .addTerm(future_PREDICATE, constants, objects, hmtypes, hm_variables )
+                        .substitute( Collections.singletonMap( TIME_PREDICATE, time_term ), constants, objects, hmtypes, hm_variables )
+                        .substitute( Collections.singletonMap( future_PREDICATE, future_term ) , constants, objects, hmtypes, hm_variables );
+                try{
+                    GRBVar grb_var = RDDL.EXPR.grb_cache.get( action_var );
+                    assert( grb_var != null );
+                    double actual = grb_var.get( GRB.DoubleAttr.X );
+
+                    //NOTE : uncomment this part if having issues with constrained actions
+                    // such as if you get -1E-11 instead of 0,
+                    //and you are expecting a positive action >= 0
+                    String interm_val = State._df.format( actual );
+                    //System.out.println( actual + " rounded to " + interm_val );
+                    Pair<RDDL.LCONST,RDDL.LCONST> key_val = new Pair<>(future_term,time_term);
+                    HashMap<RDDL.EXPR,Double> opti_values = new HashMap<>();
+                    opti_values.put(action_var,Double.valueOf(interm_val));
+                    ret.put( action_var, Double.valueOf(  interm_val ) );
+                } catch (GRBException e) {
+                    e.printStackTrace();
+                    ////System.exit(1);
+                }
+            }
+        }
+        ArrayList<RDDL.PVAR_INST_DEF> gurobi_action = new ArrayList<>();
+        for(RDDL.EXPR key : ret.keySet()){
+            Double val = ret.get(key);
+            if(key instanceof RDDL.PVAR_EXPR){
+                RDDL.PVAR_NAME p_name = ((RDDL.PVAR_EXPR) key)._pName;
+                ArrayList<RDDL.LTERM> terms = new ArrayList<RDDL.LTERM>(((RDDL.PVAR_EXPR) key)._alTerms.subList(0,2));
+                gurobi_action.add(new RDDL.PVAR_INST_DEF(p_name._sPVarName, val >0.95 ? Boolean.TRUE : Boolean.FALSE,terms));
+
+            }else{
+                throw new Exception("There should be a PVAR_EXPR");
+            }
+        }
+
+        HashSet gurobi_action_set = new HashSet(gurobi_action);
+        ArrayList<RDDL.PVAR_INST_DEF> rand_actions = random_policy.getActions(s);
+        HashSet<RDDL.PVAR_INST_DEF> random_action_set = new HashSet(rand_actions);
+        int MAX_TRAILS = 20;
+        int num_of_trails  = 0 ;
+        while(random_action_set.equals(gurobi_action_set) && num_of_trails<MAX_TRAILS){
+            random_action_set = new HashSet<RDDL.PVAR_INST_DEF>(random_policy.getActions(s));
+            num_of_trails +=1;
+        }
+
+        return random_action_set;
+    }
+
+
+    public GRBModel addBoolDiffAction(GRBModel grbModel, HashSet<RDDL.PVAR_INST_DEF> random_action_set, int future_val) throws Exception{
+        //This function adds a different action to the root node and try returns the newly added constraint.
+        RDDL.OBJECT_VAL time_term = new RDDL.OBJECT_VAL("time0");
+        RDDL.OBJECT_VAL future_term = new RDDL.OBJECT_VAL( "future" + String.valueOf(future_val));
+        for(RDDL.PVAR_INST_DEF key1 : random_action_set){
+            RDDL.EXPR act_val  = new RDDL.PVAR_EXPR(key1._sPredName._sPVarName,key1._alTerms).addTerm(TIME_PREDICATE, constants, objects, hmtypes, hm_variables )
+                    .addTerm(future_PREDICATE, constants, objects, hmtypes, hm_variables )
+                    .substitute( Collections.singletonMap( TIME_PREDICATE, time_term ), constants, objects, hmtypes, hm_variables )
+                    .substitute( Collections.singletonMap( future_PREDICATE, future_term ) , constants, objects, hmtypes, hm_variables );
+            GRBVar grb_var = RDDL.EXPR.grb_cache.get(act_val);
+
+            if(key1._oValue instanceof Boolean){
+                Boolean action_value = (Boolean)key1._oValue ? true : false;
+                RDDL.BOOL_CONST_EXPR expr_val = new RDDL.BOOL_CONST_EXPR(action_value);
+                GRBVar x2_var_rhs = expr_val.getGRBConstr(GRB.EQUAL,grbModel,constants,objects,type_map,hmtypes,hm_variables);
+                GRBConstr a =grbModel.addConstr(grb_var,GRB.EQUAL,x2_var_rhs, name_map.get(grb_var.toString()) +"="+ name_map.get(expr_val.toString()));
+                to_remove_constr.add(a);
+            }else{
+                throw new Exception("This is not boolean, Not implemented");
+            }
+
+        }
+
+        return grbModel;
+
+
+    }
 
 
     public void getExoAndEndo(State s) throws Exception {
@@ -312,10 +467,6 @@ public class HOPExplainModule extends HOPPlannerNew {
 
 
     }
-
-
-
-
 
     protected HashMap<Pair<RDDL.LCONST,RDDL.LCONST>,Map<RDDL.EXPR,Double>> extractALLInfoGurobi(final GRBModel grb_model ) throws Exception {
 
@@ -436,12 +587,6 @@ public class HOPExplainModule extends HOPPlannerNew {
         });
 
         // This is for getting reward value from Gurobi
-
-
-
-
-
-
         System.out.println( "Maximum (unscaled) bound violation : " +  + grb_model.get( GRB.DoubleAttr.BoundVio	) );
         System.out.println("Sum of (unscaled) constraint violations : " + grb_model.get( GRB.DoubleAttr.ConstrVioSum ) );
         System.out.println("Maximum integrality violation : "+ grb_model.get( GRB.DoubleAttr.IntVio ) );
@@ -533,16 +678,20 @@ public class HOPExplainModule extends HOPPlannerNew {
             String str_state = "";
             String str_action = "NOOP";
             String str_reward = "";
-            str_state = plan.get(STATE_KEY).get(TIME_TERMS.get(i)).toString().replace(",",":");
-            str_reward = plan.get(REWARD_KEY).get(TIME_TERMS.get(i)).toString().replace(",",":");
-
+            //This handles when there is no State variables.
+            try{
+                str_state = plan.get(STATE_KEY).get(TIME_TERMS.get(i)).toString().replace(",",":");
+            } catch (Exception e){
+                System.out.println("--No State Variable--");
+            }
+            //This handles action when not present
             try{
                 str_action = plan.get(ACTION_KEY).get(TIME_TERMS.get(i)).toString().replace(",",":");
 
             }catch (Exception e){
-                System.out.println("No Action Variable");
+                System.out.println("--No Action Variable--");
             }
-
+            str_reward = plan.get(REWARD_KEY).get(TIME_TERMS.get(i)).toString().replace(",",":");
             output.add(str_state);
             output.add(str_action);
             output.add(str_reward);
@@ -569,9 +718,6 @@ public class HOPExplainModule extends HOPPlannerNew {
     }
 
 
-
-
-
     public static void main(String[] args) throws Exception{
 
         //need to implement a calling function!!!..
@@ -584,6 +730,9 @@ public class HOPExplainModule extends HOPPlannerNew {
         // 7. hindsight_strat
         // 8. num_of_rounds
         // 9. rand_seed
+        // 10.Plans writing csv File
+        // 11.Prefix for writing Viz Representation.
+
 
         String rddl_file_path = args[0];
         String rddl_instance_name = args[1];
@@ -595,8 +744,10 @@ public class HOPExplainModule extends HOPPlannerNew {
         String num_of_rounds   = args[7];
         String rand_seed = args[8];
         String plan_filename  = args[9];
+        String prefix_path = args[10];
+        String explain_obj = args[11];
 
-        HOPExplainModule hop_exp = new HOPExplainModule(rddl_file_path,n_Futures,n_lookahead,rddl_instance_name,gurobi_timeout,future_gen_type,hindsight_strat,rand_seed,plan_filename);
+        HOPExplainModule hop_exp = new HOPExplainModule(rddl_file_path,n_Futures,n_lookahead,rddl_instance_name,gurobi_timeout,future_gen_type,hindsight_strat,rand_seed,plan_filename,prefix_path,explain_obj);
         hop_exp.warpperRunExplaination();
 
     }
